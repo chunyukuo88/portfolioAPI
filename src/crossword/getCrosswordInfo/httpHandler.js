82f5a378744f5797304c0a49b1eb313489a5dbaa
chunyukuo88 <alexgochenour@gmail.com>
2023-02-11 00:07:48 -0500

    chore: Updated serverless.yml

--- src/crossword/getCrosswordInfo/httpHandler.js
+++ src/crossword/getCrosswordInfo/httpHandler.js
@@ -1,15 +1,13 @@
 import { getSupabaseClient } from '../../common/factory';
 
-export async function getCrosswordInfo(logger){
+export async function getCrosswordInfo(logger) {
   logger('getCrosswordInfo()');
   const supabase = getSupabaseClient();
-  const { data, err } = await supabase
-    .from('Crossword-Solutions')
-    .select('*');
+  const { data, err } = await supabase.from('Crossword-Solutions').select('*');
   if (err) console.error('Error: ', err);
   const response = {
     statusCode: 200,
-    data
-  }
+    data,
+  };
   return response;
 }
