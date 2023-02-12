2874d7522723afb40719e36624185dd77b95ebe0
chunyukuo88 <alexgochenour@gmail.com>
2023-02-11 21:53:48 -0500

    Happy and unhappy paths for getCrosswordInfo/httpHandler().

---
+++ src/common/http.js
@@ -0,0 +1,18 @@
+export const buildSuccessResponse = (data) => ({
+  statusCode: httpStatus.SUCCESSFUL,
+  body: JSON.stringify(data),
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': '*',
+  },
+});
+
+export const buildErrorResponse = (statusCode) => ({statusCode});
+
+export const httpStatus = {
+  INTERNAL_ERROR: 500,
+  MISSING_ARGUMENT: 422,
+  SUCCESSFUL: 200,
+};
--- src/crossword/getCrosswordInfo/httpHandler.js
+++ src/crossword/getCrosswordInfo/httpHandler.js
@@ -1,16 +1,18 @@
 import * as dotenv from 'dotenv';
 import { getSupabaseClient } from '../../common/factory';
+import {buildErrorResponse, buildSuccessResponse, httpStatus} from '../../common/http';
 
 dotenv.config();
 
 export async function getCrosswordInfo() {
   console.log('getCrosswordInfo()');
   const supabase = getSupabaseClient();
-  const { data, err } = await supabase.from('Crossword-Solutions').select('*');
-  if (err) console.error('Error: ', err);
-  const response = {
-    statusCode: 200,
-    data,
-  };
-  return response;
+  try {
+    const { data } = await supabase.from('Crossword-Solutions').select('*');
+    const response = buildSuccessResponse(data);
+    return response;
+  } catch (e) {
+    return buildErrorResponse(httpStatus.INTERNAL_ERROR);
+  }
 }
+
