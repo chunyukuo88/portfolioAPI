fcf1a6dd8484769441505299fda5eb08c7bca665
chunyukuo88 <alexgochenour@gmail.com>
2023-02-12 23:26:13 -0500

    unhappy path tests for writeCrosswordInfo.

--- src/crossword/writeCrosswordInfo/httpHandler.js
+++ src/crossword/writeCrosswordInfo/httpHandler.js
@@ -5,6 +5,20 @@ import {
   httpStatus,
 } from '../../common/http';
 
+const missingArgument = (httpRequest) => (!httpRequest || !httpRequest.body);
+
 export async function writeCrosswordInfo(httpRequest) {
+  if (missingArgument(httpRequest)) return buildErrorResponse(httpStatus.MISSING_ARGUMENT);
   console.log('writeCrosswordInfo()');
+  try {
+    const supabase = getSupabaseClient();
+    const newCrosswordData = JSON.parse(httpRequest.body);
+    await supabase
+      .from('Crossword-Solutions')
+      .insert(newCrosswordData);
+    return buildSuccessResponse([]);
+  } catch (e) {
+    console.error(e);
+    return buildErrorResponse(httpStatus.INTERNAL_ERROR);
+  }
 }
