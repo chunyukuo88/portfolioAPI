65b78335d678a4735832d0ead358e41a31b3ce70
chunyukuo88 <alexgochenour@gmail.com>
2023-02-12 23:53:09 -0500

    It seems a lot was missed in the previous commit. Re-pushing.

--- src/common/factory.test.js
+++ src/common/factory.test.js
@@ -1,8 +1,8 @@
-import { getSupabaseClient } from './factory';
-import { SupabaseClient } from '@supabase/supabase-js';
+import { getSupabaseClient } from "./factory";
+import { SupabaseClient } from "@supabase/supabase-js";
 
-describe('WHEN: getSupabaseClient is invoked', () => {
-  it('THEN: it returns a Supabase client', () => {
+describe("WHEN: getSupabaseClient is invoked", () => {
+  it("THEN: it returns a Supabase client", () => {
     const client = getSupabaseClient();
 
     expect(client).toBeInstanceOf(SupabaseClient);
