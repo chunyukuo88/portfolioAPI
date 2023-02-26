fba8d4029c2dc99648c063a7356f1a7192138df3
chunyukuo88 <alexgochenour@gmail.com>
2023-02-25 23:05:54 -0500

    Not sure why Istanbul is showing lines 10 and 11 as uncovered for factory.js, but those lines are definitely accounted for in factory.test.js.

--- src/common/factory.test.js
+++ src/common/factory.test.js
@@ -1,10 +1,21 @@
-import { getSupabaseClient } from "./factory";
-import { SupabaseClient } from "@supabase/supabase-js";
+import { getSupabaseClient } from './factory';
+import {SupabaseClient} from "@supabase/supabase-js";
 
-describe("WHEN: getSupabaseClient is invoked", () => {
-  it("THEN: it returns a Supabase client", () => {
+describe('WHEN: getSupabaseClient is invoked', () => {
+  it('THEN: It returns a Supabase client', () => {
+    const expectedKeys = [
+      'supabaseUrl', 'supabaseKey',
+      'realtimeUrl', 'authUrl',
+      'storageUrl',  'functionsUrl',
+      'storageKey',  'headers',
+      'auth',        'fetch',
+      'realtime',    'rest'
+    ];
     const client = getSupabaseClient();
 
     expect(client).toBeInstanceOf(SupabaseClient);
+    expectedKeys.forEach(key => {
+      expect(client).toHaveProperty(key);
+    });
   });
 });
