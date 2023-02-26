f5526ede3a59e5f36be4baada2dfeae89f0d7622
chunyukuo88 <alexgochenour@gmail.com>
2023-02-25 23:20:38 -0500

    Previous commit failed.

--- src/crossword/getCrosswordInfo/httpHandler.js
+++ src/crossword/getCrosswordInfo/httpHandler.js
@@ -9,7 +9,9 @@ export async function getCrosswordInfo() {
   console.log('getCrosswordInfo()');
   const supabase = getSupabaseClient();
   try {
-    const { data } = await supabase.from(process.env.SUPABASE_CROSSWORD_TABLE).select('*');
+    const { data } = await supabase
+      .from(process.env.SUPABASE_CROSSWORD_TABLE)
+      .select('*');
     const response = buildSuccessResponse(data);
     return response;
   } catch (e) {
