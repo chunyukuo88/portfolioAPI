f365c4c7388171757e23e5e2b6ef8d6aae488741
chunyukuo88 <alexgochenour@gmail.com>
2023-03-03 20:25:46 -0500

    All tests done for getBlogPosts/httpHandler.handler

--- blog/src/getBlogPosts/httpHandler.js
+++ blog/src/getBlogPosts/httpHandler.js
@@ -3,7 +3,15 @@ import {
   buildSuccessResponse,
   httpStatus,
 } from '../common/http';
+import { getAllEntries } from './getAllEntries';
 
-export async function getAllEntries(httpRequest) {
-
+export async function handler(httpRequest) {
+  const { log } = console;
+  log('getBlogPosts/httpHandler.handler()');
+  try {
+    const data = await getAllEntries(httpRequest);
+    return buildSuccessResponse(data);
+  } catch (e) {
+    return buildErrorResponse(httpStatus.INTERNAL_ERROR);
+  }
 }
--- blog/src/getBlogPosts/httpHandler.test.js
+++ blog/src/getBlogPosts/httpHandler.test.js
@@ -3,15 +3,20 @@ import {
   httpStatus,
   standardHeaders
 } from '../common/http';
-import { getAllEntries } from './httpHandler';
+import { handler } from './httpHandler';
+import { getAllEntries } from './getAllEntries';
 
-jest.mock('./httpHandler');
+jest.mock('./getAllEntries');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('getAllEntries/0', () => {
-  describe('GIVEN: A valid HTTP request,', () => {
-    describe('WHEN: this function is invoked', () => {
+  describe('GIVEN: there are no problems with the Redis server,', () => {
+    describe('WHEN: this function is invoked with a valid HTTP request,', () => {
       it('THEN: it returns a success response.', async () => {
-        getAllEntries.mockReturnValueOnce([
+        const expectedData = [
           {
             entityId: '01GTM5P7XKK1DFEGACMBZNDH4K',
             imageUrl: '',
@@ -28,16 +33,59 @@ describe('getAllEntries/0', () => {
             title: 'Bloggimus',
             views: 0,
           },
-        ]);
+        ];
+        getAllEntries.mockReturnValueOnce(expectedData);
 
         const httpRequest = {};
         httpRequest.pathParameters = {};
 
-        const expectedResponse = {};
+        const expectedResponse = {
+          statusCode: httpStatus.SUCCESSFUL,
+          body: JSON.stringify(expectedData),
+          headers: standardHeaders,
+        };
 
-        const response = await getAllEntries();
+        const response = await handler(httpRequest);
 
         expect(response).toEqual(expectedResponse);
+      });
+    });
+  });
+  describe('GIVEN: there is a problem with the Redis server,', () => {
+    describe('WHEN: this function is invoked with a valid HTTP request,', () => {
+      it('THEN: it returns an error response with a 500 status.', async () => {
+        const expectedErrorMsg = 'Redis server is down.';
+        getAllEntries.mockRejectedValueOnce(new Error(expectedErrorMsg));
+
+        const httpRequest = {};
+        httpRequest.pathParameters = {};
+
+        const expectedResponse = {
+          statusCode: httpStatus.INTERNAL_ERROR,
+          body: JSON.stringify({
+            error: {
+              errorMessage: errorMessages.INVALID_REQUEST,
+            },
+          }),
+          headers: standardHeaders,
+        };
+
+        const response = await handler(httpRequest);
+
+        expect(response).toEqual(expectedResponse);
+      });
+    });
+  });
+  describe('GIVEN: any state of the Redis server, up or down,', () => {
+    describe('WHEN: this handler is invoked,', () => {
+      it('THEN: its invocation is logged.', async () => {
+        const spy = jest.spyOn(console, 'log').mockImplementationOnce(jest.fn());
+        const httpRequest = {};
+        httpRequest.pathParameters = {};
+
+        await handler(httpRequest);
+
+        expect(spy).toBeCalledTimes(1);
       });
     });
   });
