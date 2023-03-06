4c0e30287cf4efa543ceaa0d08062aa8da3241f8
chunyukuo88 <alexgochenour@gmail.com>
2023-03-06 13:38:43 -0500

    Deploying to manually test ID signature.

--- blog/src/deleteBlogPost/deletePost.js
+++ blog/src/deleteBlogPost/deletePost.js
@@ -1,3 +1,9 @@
+import { getRepository } from '../common/repository';
+
 export async function deletePost(blogPostId) {
   console.log(blogPostId);
+  const { blogPostRepository, client } = await getRepository();
+  await blogPostRepository.remove(blogPostId);
+  await client.close();
+  return blogPostId;
 }
---
+++ blog/src/deleteBlogPost/deletePost.test.js
@@ -0,0 +1,15 @@
+import { deletePost } from './deletePost';
+
+describe('deletePost/1', () => {
+  describe('GIVEN: A valid blogPostId', () => {
+    describe('WHEN: this function is invoked,', () => {
+      it('THEN: deletes that blog entry.', async () => {
+        const blogPostId = 'BlogPost:01GTM5P7XKK1DFEGACMBZNDH4J';
+
+        const result = await deletePost(blogPostId);
+
+        expect(result).toEqual(blogPostId);
+      });
+    });
+  });
+});
