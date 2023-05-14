657b857983eb43ac1a4369b067102f23163163e4
chunyukuo88 <alexgochenour@gmail.com>
2023-05-14 16:19:57 -0400

    Created test for new feature's logic function. Next: Get test to pass.

---
+++ blog/src/getBlogsByPage/getByPage.test.js
@@ -0,0 +1,44 @@
+import { getByPage } from "./getByPage";
+import { getRepository } from "../common/repository";
+
+jest.mock("../common/repository", () => ({
+    getRepository: jest.fn(),
+}));
+
+describe('getByPage/1', function () {
+    describe('GIVEN: A page number,', () => {
+       describe('WHEN: This function is invoked,', () =>{
+           it('THEN: returns an array of blog entry objects containing that exact page number', async () => {
+               const mockBlogEntries = [
+                   {
+                       entityId: "01GTM5P7XKK1DFEGACMBZNDH4J",
+                       imageUrl: "",
+                       likes: 0,
+                       theme: "Page 2 Posts",
+                       title: "A Walk on the Mild Side",
+                       views: 0,
+                       page: 2,
+                   },
+               ];
+               getRepository.mockResolvedValue({
+                   blogPostRepository: {
+                       search: () => ({
+                           return: {
+                               all: () => mockBlogEntries,
+                           },
+                           search: jest.fn().mockReturnThis(),
+                       }),
+                       createIndex: jest.fn(),
+                   },
+                   client: {
+                       close: jest.fn(),
+                   },
+               });
+
+               const result = await getByPage(2);
+
+               expect(result).toEqual(mockBlogEntries);
+           });
+       });
+    });
+});
