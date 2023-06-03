6f8ee0c4bc803c98e4003a5049d90461ee1a2a1f
chunyukuo88 <alexgochenour@gmail.com>
2023-06-02 23:25:03 -0400

    Added a lot of missing tests for createEntry.js unhappy paths.

--- blog/src/writeBlogPost/createEntry.js
+++ blog/src/writeBlogPost/createEntry.js
@@ -1,8 +1,22 @@
 import { getRepository } from '../common/repository';
+import { buildErrorResponse, httpStatus } from '../common/http';
+
+const invalidData = (blogData) => (
+  !blogData
+  || !blogData.title
+  || !blogData.creationTimeStamp
+  || !blogData.theme
+  || !blogData.imageUrl
+);
 
 export async function createEntry(blogData) {
+  console.log('createEntry()');
+  if (invalidData(blogData)) {
+    return buildErrorResponse(httpStatus.MISSING_ARGUMENT);
+  }
   const { blogPostRepository, client } = await getRepository();
   const newEntity = await blogPostRepository.createAndSave(blogData);
+  console.log('createEntry() - newEntity:', newEntity);
   await client.close();
   return newEntity;
 }
--- blog/src/writeBlogPost/createEntry.test.js
+++ blog/src/writeBlogPost/createEntry.test.js
@@ -1,36 +1,144 @@
 import { createEntry } from "./createEntry";
 import { getRepository } from "../common/repository";
+import {buildErrorResponse, httpStatus} from "../common/http";
 
 jest.mock("../common/repository");
 
-describe("GIVEN: valid blog data,", () => {
-  describe("WHEN: this function is invoked,", () => {
-    it("THEN: it returns a new blog entity, which is returned to the handler.", async () => {
-      const mockBlogEntity = {
-        foo: "bar",
-      };
-      getRepository.mockReturnValueOnce({
-        blogPostRepository: {
-          createAndSave: () => {
-            return mockBlogEntity;
+jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+describe("createEntry()", () => {
+  describe("GIVEN: valid blog data,", () => {
+    describe("WHEN: this function is invoked,", () => {
+      it("THEN: it returns a new blog entity, which is returned to the handler.", async () => {
+        const mockBlogEntity = {
+          foo: "bar",
+        };
+        getRepository.mockReturnValueOnce({
+          blogPostRepository: {
+            createAndSave: () => {
+              return mockBlogEntity;
+            },
           },
-        },
-        client: {
-          close: jest.fn(),
-        },
+          client: {
+            close: jest.fn(),
+          },
+        });
+        const blogData = {
+          title: "test",
+          creationTimeStamp: '1679409635239',
+          theme: "test",
+          imageUrl: "test",
+          likes: 0,
+          views: 0,
+        };
+
+        const result = await createEntry(blogData);
+
+        expect(result).toEqual(mockBlogEntity);
       });
-      const blogData = {
-        title: "test",
-        creationTimeStamp: '1679409635239',
-        theme: "test",
-        imageUrl: "test",
-        likes: 0,
-        views: 0,
-      };
+    });
+  });
+  describe("UNHAPPY PATHS: ", () => {
+    describe("GIVEN: missing blog data,", () => {
+      describe("WHEN: this function is invoked,", () => {
+        it("THEN: it returns an error, which is returned to the handler.", async () => {
+          const expectedError = new Error(
+            "There was a problem removing the blog post."
+          );
+          getRepository.mockReturnValueOnce({
+            blogPostRepository: {
+              createAndSave: () => {
+                throw expectedError;
+              },
+            },
+            client: {
+              close: jest.fn(),
+            },
+          });
+          const blogData = undefined;
 
-      const result = await createEntry(blogData);
+          const result = await createEntry(blogData);
 
-      expect(result).toEqual(mockBlogEntity);
+          expect(result).toEqual(buildErrorResponse(httpStatus.MISSING_ARGUMENT));
+        });
+      });
+    });
+    describe("GIVEN: null blog data,", () => {
+      describe("WHEN: this function is invoked,", () => {
+        it("THEN: it returns an error, which is returned to the handler.", async () => {
+          const expectedError = new Error(
+            "There was a problem removing the blog post."
+          );
+          getRepository.mockReturnValueOnce({
+            blogPostRepository: {
+              createAndSave: () => {
+                throw expectedError;
+              },
+            },
+            client: {
+              close: jest.fn(),
+            },
+          });
+          const blogData = null;
+
+          const result = await createEntry(blogData);
+
+          expect(result).toEqual(buildErrorResponse(httpStatus.MISSING_ARGUMENT));
+        });
+      });
+    });
+    describe("GIVEN: empty blog data,", () => {
+      describe("WHEN: this function is invoked,", () => {
+        it("THEN: it returns an error, which is returned to the handler.", async () => {
+          const expectedError = new Error(
+            "There was a problem removing the blog post."
+          );
+          getRepository.mockReturnValueOnce({
+            blogPostRepository: {
+              createAndSave: () => {
+                throw expectedError;
+              },
+            },
+            client: {
+              close: jest.fn(),
+            },
+          });
+          const blogData = {};
+
+          const result = await createEntry(blogData);
+
+          expect(result).toEqual(buildErrorResponse(httpStatus.MISSING_ARGUMENT));
+        });
+      });
+    });
+    describe("GIVEN: blog data that is not empty but is missing fields,", () => {
+      describe("WHEN: this function is invoked,", () => {
+        it("THEN: it returns an error, which is returned to the handler.", async () => {
+          const expectedError = new Error(
+            "There was a problem removing the blog post."
+          );
+          getRepository.mockReturnValueOnce({
+            blogPostRepository: {
+              createAndSave: () => {
+                throw expectedError;
+              },
+            },
+            client: {
+              close: jest.fn(),
+            },
+          });
+          const blogData = {
+            title: 'A valid title',
+            creationTimeStamp: null,
+            theme: undefined,
+            imageUrl: 'www.example.com',
+          };
+
+          const result = await createEntry(blogData);
+
+          expect(result).toEqual(buildErrorResponse(httpStatus.MISSING_ARGUMENT));
+        });
+      });
     });
   });
 });
