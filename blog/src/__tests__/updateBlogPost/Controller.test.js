1f0a70248c8bfff7877d7b0afb9ff96371bb35f1
chunyukuo88 <alexgochenour@gmail.com>
2023-10-16 20:49:18 -0400

    Added docs to README.md file.

---
+++ blog/src/__tests__/updateBlogPost/Controller.test.js
@@ -0,0 +1,12 @@
+describe('WHEN: given an ID and a payload,', () => {
+  it('THEN: updates the entire entry with a new `results` value.', () => {
+    const id = 4;
+    const payload = {
+      "body": "Page 4, article 12",
+      "title": "Test 12",
+      "imageUrl": "https://czzbyiyicvjcorsepbfp.supabase.co/storage/v1/object/public/alexgochenour.xyz-blog-photos/z_Berry%20Bread-min.JPG",
+    };
+
+
+  });
+});
