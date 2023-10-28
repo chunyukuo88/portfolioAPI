fff8d1c290bb80b61d224bb56649c6450c74ef00
chunyukuo88 <alexgochenour@gmail.com>
2023-10-27 23:12:52 -0400

    Corrected an error string.

--- blog/src/createArticle/httpHandler.js
+++ blog/src/createArticle/httpHandler.js
@@ -9,11 +9,11 @@ export async function handler(httpRequest) {
   const { log, error } = console;
   log('createArticle/httpHandler.handler()');
   try {
-    const newBlogArticle = httpRequest.body;
+    const newBlogArticle = JSON.parse(httpRequest.body);
     await createArticle(newBlogArticle);
     return buildSuccessResponse(newBlogArticle);
   } catch (e) {
-    error('getBlogPosts/httpHandler.handler() - error: ', e);
+    error('createArticle/httpHandler.handler() - error: ', e);
     return buildErrorResponse(httpStatus.INTERNAL_ERROR);
   }
 }
