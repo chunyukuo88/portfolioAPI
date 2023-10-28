68145dcf41dcf775be30020bbc632ab9b1878aa6
chunyukuo88 <alexgochenour@gmail.com>
2023-10-28 18:09:08 -0400

    Improved readability of Article model.

--- blog/src/common/models/Article.js
+++ blog/src/common/models/Article.js
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export function Article(title, imageUrl, body, page) {
-  this.title = title; // string
-  this.imageUrl = imageUrl; // string
-  this.body = body; // string
+  this.title = title;
+  this.imageUrl = imageUrl;
+  this.body = body;
   this.page = page // number
 
   this.articleId = uuidv4(); // string
-  this.likes = 1; // number
-  this.views = 1; // number
+  this.likes = 1;
+  this.views = 1;
 }
